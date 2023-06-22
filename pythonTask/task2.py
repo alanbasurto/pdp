0e01b3d0744e89a4de8fff1a1a679879d83e3b8d
Alan Basurto <alanrbasurto@gmail.com>
2023-06-22 07:27:00 -0600

    update task2

--- pythonTask/task2.py
+++ pythonTask/task2.py
@@ -2,15 +2,16 @@ import os
 from datetime import datetime
 
 def main():
-    filename="thefile.json"
+    filename="thefile.txt"
     filesize=os.path.getsize(filename)
-    if filesize/1000000> 10:
+    if filesize/1048576> .10:
         now = datetime.now()
-        newfilename = filename.split(".")[0] + now.strftime("%d%m%Y-%H:%M:%S") + ".json"
+        newfilename = filename.split(".")[0] + now.strftime("%d%m%Y-%H:%M:%S") + "."+filename.split(".")[1]
         os.rename(filename,newfilename)
         f = open(filename,"w+")
         f.close()
     print(filesize,"bytes")
+    print(filesize/1048576,"mb")
 
 if __name__ == "__main__":
     main()
